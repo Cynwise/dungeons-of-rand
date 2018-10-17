bec0b556d4c64dac3c426feaf3dc037d8539bdc5
MeghOilwala <meghoilwala@gmail.com>
2018-10-16 21:42:58 -0400

    Adding Slime actor

---
+++ actor/slime.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file slime.cpp
+ */
+
+#include <actor/slime.h>
+#include <rng.h>
+
+Slime::Slime()
+{
+        std::string slime_color[4] = {"Green Slime","Blue Slime","Red Slime","Black Slime"};
+        int x = rng(0,4);
+
+    name = slime_color[x];
+    level = 1;
+    max_hp = 5;
+    hp = max_hp;
+}
+
+Slime* Slime::clone()
+{
+    Slime* tmp = new Slime;
+    *tmp = *this;
+    return tmp;
+}
